38d599cfa7511d8513d4d11cca64e7d63e36e795
Bhanu Prakash Bhukya <bhanuprakash@aititude.in>
2024-07-27 01:05:39 +0530

    Implemented Sign Up page.

--- src/app/log-in/login/login.component.ts
+++ src/app/log-in/login/login.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  @Input() showModal: boolean = false;
 
+  openSignUp() {
+    this.showModal = true;
+  }
 
+  close(): void {
+    this.showModal = false;
+  }
+
+  onSubmit(): void {
+    // Handle form submission logic here
+    console.log('Form submitted');
+    this.close();
+  }
 
 }
