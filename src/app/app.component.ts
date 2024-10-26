25e423032bca781cbee936a96ca0e90173bf82d2
Bhanu Prakash Bhukya <161059701+BhanuPrakashBhukya@users.noreply.github.com>
2024-10-26 22:25:51 +0530

    ngx-spinner is added (#4) (#5)
    Co-authored-by: Bhukya Bhanu Prakash <bhanuprakash8082@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginComponent } from './log-in/login/login.component';
 import { AuthService } from './common/auth.service';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home/home-page/home-page.component';
+import { NgxSpinnerModule } from 'ngx-spinner'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [  CommonModule, LoginComponent, HomePageComponent ],
+  imports: [  CommonModule, LoginComponent, HomePageComponent, NgxSpinnerModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { LoginModule } from './log-in/modules/login.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    importProvidersFrom(CommonModule, BrowserModule, HttpClientModule, LoginModule),
+    importProvidersFrom(CommonModule, BrowserModule, NgxSpinnerModule, HttpClientModule, LoginModule),
     provideHttpClient(withFetch())
   ],
 };
